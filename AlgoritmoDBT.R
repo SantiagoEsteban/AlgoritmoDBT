c2d8393f6c2deb26e0b539a6a5a7f139a631edb9
SantiagoEsteban <santiagoesteban@gmail.com>
2016-07-20 16:43:13 -0300

    Terminado base final training set.

--- AlgoritmoDBT.R
+++ AlgoritmoDBT.R
@@ -10,22 +10,20 @@ library(stringr)
 #Read in data
 DBT <- read_excel('PMA0719336_ScoreRiesgoCV_Corr_20150715 - FINAL CON OUTCOMES -Missing 2.xlsx')
 DBT <- select(DBT, -Comentarios, -Total_con_guardia, -Total_sin_guardia, -CANT_GLU_GUARDIA_FR, -CANT_GLU_GUARDIA_R)
-DBT$DBT_Alg_Missing <- make.names(DBT$DBT_Alg_Missing)
-DBT <- DBT[-1664,] %>% select(-DBT_Alg_Missing, -Result_Alg_Missing, -Missing_DBT_Manual)
+DBT$DBT_Manual <- make.names(DBT$DBT_Manual)
+DBT <- DBT[-1664,]
 
 #Seleccionar solo aquellos que no tienen datos
-DBT$total <- select(DBT, 3:11) %>% rowSums()
+DBT$total <- select(DBT, 4:12) %>% rowSums()
 
 #Seleccionar solo aquellas evoluciones de quienes no tienen datos
 missing <- filter(DBT, total==0) %>% select(ID_PACIENTE)
-evol_missing <- read_excel('pma0719336_evol_dbt_glu_2000_2005.xlsx')
-evol_missing <- select(evol_missing, -ESTADO, -DIAGNOSTICO, -FULLYSPECIFIEDNAME)
+evol_missing <- read_excel('pma0719336_dbt_01_05_evoluc.xls')
+evol_missing <- select(evol_missing, -ESTADO)
 evol_missing <- filter(evol_missing, FECHA < '2005-01-01')
 evol_missing2 <- inner_join(evol_missing, missing, by='ID_PACIENTE')
 #nmissing <- evol_missing2 %>% group_by(ID_PACIENTE) %>% summarise(n=n())
 
-
-#evol_missing <- as.data.frame(evol_missing$ID_PACIENTE[missing$ID_PACIENTE])
 
 #HASTA ACA
 evol_missing2$glu1 <- as.numeric(str_extract_all(evol_missing2$TEXTO, regex("(?<=(?i)glu)[0-9]+", ignore.case=TRUE)))
@@ -42,51 +40,32 @@ evol_missing2$glu11 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i
 evol_missing2$glu12 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gluc )[0-9]+", ignore.case=TRUE)))
 evol_missing2$glu13 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gluc. )[0-9]+", ignore.case=TRUE)))
 evol_missing2$glu14 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gluc:  )[0-9]+", ignore.case=TRUE)))
-evol_missing2$glu15 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)glu:)[0-9]+", ignore.case=TRUE)))
-evol_missing2$glu16 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gluc:)[0-9]+", ignore.case=TRUE)))
-evol_missing2$glu17 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)glucemia:)[0-9]+", ignore.case=TRUE)))
-evol_missing2$glu_normal <- str_detect(evol_missing2$TEXTO, regex("(?i)glucemia normal|(?i)glucemia es normal|(?i)glucemia sp|(?i)glucemia s\\/p|glu normal|(?i)glu sp|(?i)glu s\\/p", 
+evol_missing2$glu15 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gl)[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu16 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gl )[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu17 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gl\\. )[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu18 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gl:  )[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu19 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)glu:)[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu20 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gluc:)[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu21 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)glucemia:)[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu22 <-as.numeric(str_extract(evol_missing2$TEXTO, regex("(?<=(?i)gl:)[0-9]+", ignore.case=TRUE)))
+evol_missing2$glu_normal <- str_detect(evol_missing2$TEXTO, regex("glucemia normal|glucemia es normal|glucemia sp|glucemia s\\/p|glu normal|gl sp|glu sp|glu s\\/p", 
                                                                         ignore.case=TRUE))
 evol_missing2$glu_normal <- ifelse(evol_missing2$glu_normal==TRUE,1,0)
-evol_missing2$glu_total <- rowMeans(select(evol_missing2, 4:20), na.rm=T)
+evol_missing2$glu_total <- rowMeans(select(evol_missing2, 5:26), na.rm=T)
 evol_missing2 <- select(evol_missing2, ID_PACIENTE, FECHA, glu_total, glu_normal)
 evol_missing2$glu_total[evol_missing2$glu_total=='NaN']<- NA
 evol_missing2$glu_total_FR_r <- ifelse(evol_missing2$glu_total>=126, 1, 0)
 evol_missing2$glu_total_R_r <- ifelse(evol_missing2$glu_total<126, 1, 0)
+evol_missing2$glu_total_R_r <- rowSums(select(evol_missing2, glu_total_R_r, glu_normal), na.rm=T)
 evol_missing2 <- group_by(select(evol_missing2, ID_PACIENTE, glu_total_FR_r, glu_total_R_r), ID_PACIENTE) %>% 
     summarise(glu_total_FR=sum(glu_total_FR_r, na.rm=T), glu_total_R=sum(glu_total_R_r, na.rm=T))
 
-evol_missing$glu1 <- as.numeric(str_extract_all(evol_missing$TEXTO, "(?<=(?i)glu)[0-9]+"))
-evol_missing$glu2 <- as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glu )[0-9]+"))
-evol_missing$glu3 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glu  )[0-9]+"))
-evol_missing$glu4 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glu: )[0-9]+"))
-evol_missing$glu5 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glu:  )[0-9]+"))
-evol_missing$glu6 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia)[0-9]+"))
-evol_missing$glu7 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia )[0-9]+"))
-evol_missing$glu8 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia  )[0-9]+"))
-evol_missing$glu9 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia: )[0-9]+"))
-evol_missing$glu10 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia:  )[0-9]+"))
-evol_missing$glu11 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)gluc)[0-9]+"))
-evol_missing$glu12 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)gluc )[0-9]+"))
-evol_missing$glu13 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)gluc. )[0-9]+"))
-evol_missing$glu14 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)gluc:  )[0-9]+"))
-evol_missing$glu15 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glu:)[0-9]+"))
-evol_missing$glu16 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)gluc:)[0-9]+"))
-evol_missing$glu17 <-as.numeric(str_extract(evol_missing$TEXTO, "(?<=(?i)glucemia:)[0-9]+"))
-evol_missing$glu_normal <- str_detect(evol_missing$TEXTO, "(?i)glucemia normal|(?i)glucemia es normal|(?i)glucemia sp|(?i)glucemia s\\/p|glu normal|(?i)glu sp|(?i)glu s\\/p")
-evol_missing$glu_normal <- ifelse(evol_missing$glu_normal==TRUE,1,0)
-evol_missing$glu_total <- rowMeans(select(evol_missing, 4:20), na.rm=T)
-evol_missing <- select(evol_missing, ID_PACIENTE, FECHA, glu_total, glu_normal)
-evol_missing$glu_total[evol_missing$glu_total=='NaN']<- NA
-evol_missing$glu_total_FR_r <- ifelse(evol_missing$glu_total>=126, 1, 0)
-evol_missing$glu_total_R_r <- ifelse(evol_missing$glu_total<126, 1, 0)
-evol_missing <- group_by(select(evol_missing, ID_PACIENTE, glu_total_FR_r, glu_total_R_r), ID_PACIENTE) %>% 
-    summarise(glu_total_FR=sum(glu_total_FR_r), glu_total_R=sum(glu_total_R_r))
 
 #Joining
 DBT <- left_join(DBT, evol_missing2, by='ID_PACIENTE') %>% replace_na(list(glu_total_FR=0, glu_total_R=0))
 DBT$CANT_GLU_AMB_FR <- rowSums(select(DBT, CANT_GLU_AMB_FR, glu_total_FR))
 DBT$CANT_GLU_AMB_R <- rowSums(select(DBT, CANT_GLU_AMB_R, glu_total_R))
+DBT <- select(DBT, -total, -glu_total_FR, -glu_total_R, -Total_sin_guardia)
 #VOLAR ULTIMAS DOS COLUMNAS
 
 #Describe
